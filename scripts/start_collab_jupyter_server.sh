a8ee935ec7be4209f855dd7b1de8788550ac7505
Andreas Paepcke <paepcke@quintus.stanford.edu>
2020-05-29 12:34:39 -0700

    Version updates.

--- scripts/start_collab_jupyter_server.sh
+++ scripts/start_collab_jupyter_server.sh
@@ -6,13 +6,13 @@
 # b/c IPython versions conflict. However, pip installing
 # directly works. Check whether already installed:
 
-MODULE_EXISTS=$(pip list | grep google-colab)
-if [[ -z $MODULE_EXISTS ]]
-then
-    echo "Installing google-colab..."
-    pip install google-colab
-    echo "Installing google-colab maybe done."    
-fi
+# MODULE_EXISTS=$(pip list | grep google-colab)
+# if [[ -z $MODULE_EXISTS ]]
+# then
+#     echo "Installing google-colab..."
+#     pip install google-colab
+#     echo "Installing google-colab maybe done."    
+# fi
 
 
 jupyter serverextension enable --py jupyter_http_over_ws
