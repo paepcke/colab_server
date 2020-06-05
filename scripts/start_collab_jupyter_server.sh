e7cc6d960d1397a7b0b442dde8cfb820f358faaa
Andreas Paepcke <paepcke@cs.stanford.edu>
2020-06-05 11:12:35 -0700

    Added server status and stop scripts

--- scripts/start_collab_jupyter_server.sh
+++ scripts/start_collab_jupyter_server.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
-# Allow web socket connections to the server:
+SCRIPT_DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )
+PIDFILE=$SCRIPT_DIR/colab_server.pid
 
 # Using setup.py installation for google-colab fails,
 # b/c IPython versions conflict. However, pip installing
@@ -20,8 +21,14 @@ export PYTHONPATH=$(pwd)/src/bert_training/
 
 # Start the server:
 jupyter notebook \
+  --no-browser \
   --NotebookApp.allow_origin='https://colab.research.google.com' \
   --port=8888 \
   --NotebookApp.port_retries=0 \
-  > colab_server.log 2>&1
+  > colab_server.log 2>&1 &
+
+# Get the server's pid:
+PID=$!
+echo $PID > $PIDFILE
+
 
